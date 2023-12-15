c8ea3b74a614959dd65a0ad33eb69caad6f7cf65
) Rebel <irenerukwaro9@gmail.com>
2023-12-16 02:00:14 +0300

    Api integration for the search engine

--- src/script.js
+++ src/script.js
@@ -1,9 +1,24 @@
 function weatherCity(event){
   event.preventDefault();
-  let city=document.querySelector("h1");
   let cityInput=document.querySelector("#entry-search");
-  city.innerHTML=cityInput.value;
+  apiSearch(cityInput.value);
 }
-
 let form=document.querySelector("#inquiry-form");
 form.addEventListener("submit", weatherCity);
+
+function apiSearch(city){
+  let apiKey= "2a73b3dbc21aoe20a5dfta3e447dfa66";
+  let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+apiSearch("Nairobi"); //default city with correct temperature
+
+function displayWeather(response){
+  let city=document.querySelector("h1");
+  city.innerHTML=response.data.city;
+  let degree=document.querySelector("#degree");
+  let temperature=Math.round(response.data.temperature.current)
+  degree.innerHTML=temperature;
+}
+
