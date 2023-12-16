86f22b0aace604b620266e96326f81c036a9ba5d
) Rebel <irenerukwaro9@gmail.com>
2023-12-16 04:00:48 +0300

    included real time data

--- src/script.js
+++ src/script.js
@@ -20,5 +20,34 @@ function displayWeather(response){
   let degree=document.querySelector("#degree");
   let temperature=Math.round(response.data.temperature.current)
   degree.innerHTML=temperature;
+
+  let humidity=document.querySelector("#humidity");
+  humidity.innerHTML=`${response.data.temperature.humidity}%`;
+  let windspeed=document.querySelector("#windspeed");
+  windspeed.innerHTML=`${response.data.wind.speed}km/h`;
+  let description=document.querySelector("#description");
+  description.innerHTML=response.data.condition.description;
+  let icon=document.querySelector("#weather-icon");
+  icon.innerHTML=`<img src=${response.data.condition.icon_url} alt=${response.data.condition.icon}>`;
+
+  let date=new Date();
+  let dayInfo=document.querySelector("#dayInfo");
+  dayInfo.innerHTML=displayDay(date);
+
+  let time=document.querySelector("#time");
+  time.innerHTML=displayTime(date);
 }
 
+function displayDay(info){
+ let today=info.getDay();
+ let days=["Sunday", "Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"];
+ today=days[info.getDay()];
+ return today;
+}
+
+function displayTime(info){
+  let hours=info.getHours().toString().padStart(2, "0");
+  let minutes=info.getMinutes().toString().padStart(2, "0");
+
+  return `${hours}:${minutes}`;
+}
